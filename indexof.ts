e67ce4abe58e0c79efa492a74cf21808925cd2c5
rajayaseen <yaseenraja340@gmail.com>
2023-09-14 00:18:31 -0700

    added array method

---
+++ indexof.ts
@@ -0,0 +1,61 @@
+// indexOf()
+// let array= ['a','b','c','d'];
+// console.log(array.indexOf('c')!== -1);
+
+// lastIindexOf()
+// let number:string [] =[];
+// console.log("Last Index Of 5 before: ", number.lastIndexOf('5'));
+// number=['1','2','3','4']
+// console.log("Last Index Of 5 after: ", number.lastIndexOf('5'));
+
+// concat()
+// const days:string[]=["mon","tue","wed","thur"]
+// let month:string[]= ["jan","feb","mar","apr"]
+// console.log("result :",days.concat(month))
+
+// join()
+// let colours=["red","black","blue"]
+// console.log("result :", colours.join("1"));
+
+// push()
+// let mobiles=["iphone","itel","samsung"]
+// mobiles.push("tecno")
+// console.log("pushed tecno mobile: ", mobiles);
+// mobiles.push("nokia")
+// console.log("pushed nokia mobile: ", mobiles);
+
+// pop()
+// let cars=["civic","alto","mehran","GLI"]
+// cars.pop()
+// console.log("popped car: ", cars);
+// cars.pop()
+// console.log("popped car: ", cars);
+
+// reverse()
+//  let bikes=["CD70","125","union","ghani"]
+// console.log("reverse array: ", bikes.reverse());
+
+//shift()
+// let cricketitems=["bat","bowl","wickets"]
+// const shifteditems=cricketitems.shift()
+// console.log("result array: ", cricketitems);
+// console.log("shifted items: ", shifteditems);
+
+//unshift()
+// let stationary=["pencils","books","copies"]
+// stationary.unshift("sheets")
+// console.log("unshifted sheets: ", stationary);
+
+//slice()
+// let Electricitems=["fans","chargers","computers","Digital camera","electric drill"]
+// console.log("result: ", Electricitems.slice(1,4));
+
+
+
+
+
+
+
+
+
+
