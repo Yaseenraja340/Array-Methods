c6b9679284dd9b37e279563347185a64f1416dbe
rajayaseen <yaseenraja340@gmail.com>
2023-09-14 23:47:14 -0700

    Arrays Method done

--- indexof.ts
+++ indexof.ts
@@ -1,57 +1,57 @@
-// indexOf()
+//1. indexOf()
 // let array= ['a','b','c','d'];
 // console.log(array.indexOf('c')!== -1);
 
-// lastIindexOf()
+//2. lastIindexOf()
 // let number:string [] =[];
 // console.log("Last Index Of 5 before: ", number.lastIndexOf('5'));
 // number=['1','2','3','4']
 // console.log("Last Index Of 5 after: ", number.lastIndexOf('5'));
 
-// concat()
+//3. concat()
 // const days:string[]=["mon","tue","wed","thur"]
 // let month:string[]= ["jan","feb","mar","apr"]
 // console.log("result :",days.concat(month))
 
-// join()
+//4. join()
 // let colours=["red","black","blue"]
 // console.log("result :", colours.join("1"));
 
-// push()
+//5. push()
 // let mobiles=["iphone","itel","samsung"]
 // mobiles.push("tecno")
 // console.log("pushed tecno mobile: ", mobiles);
 // mobiles.push("nokia")
 // console.log("pushed nokia mobile: ", mobiles);
 
-// pop()
+//6. pop()
 // let cars=["civic","alto","mehran","GLI"]
 // cars.pop()
 // console.log("popped car: ", cars);
 // cars.pop()
 // console.log("popped car: ", cars);
 
-// reverse()
+//7. reverse()
 //  let bikes=["CD70","125","union","ghani"]
 // console.log("reverse array: ", bikes.reverse());
 
-//shift()
+//8. shift()
 // let cricketitems=["bat","bowl","wickets"]
 // const shifteditems=cricketitems.shift()
 // console.log("result array: ", cricketitems);
 // console.log("shifted items: ", shifteditems);
 
-//unshift()
+//9. unshift()
 // let stationary=["pencils","books","copies"]
 // stationary.unshift("sheets")
 // console.log("unshifted sheets: ", stationary);
 
-//slice()
+//10. slice()
 // let Electricitems=["fans","chargers","computers","Digital camera","electric drill"]
 // console.log("result: ", Electricitems.slice(1,4));
 
 
-//splice()
+//11. splice()
 // let citynames=["islamabad","karachi","multan","rawalpindi"]
 // citynames.splice(4,1,'queta')//added cityname
 // console.log("added city name: ", citynames);
@@ -60,11 +60,11 @@
 // citynames.splice(2)//removed cityname
 // console.log("removed city name: ", citynames);
 
-//tostring()
+//12. tostring()
 // let countrynames=['pakistan','india','china','iran']
 // console.log("string from country names: ", countrynames.toString());
 
-//filter()
+//13. filter()
 // let myfriends=['rafay','rasheed','shehzad','haider','ramzan','haseeb']
 // let myczns=['haseeb','saad','ahsan','waheed','ali','haseeb','waheed','saad']
 // let result=myfriends.filter(f=> myczns.indexOf(f)!== -1)
@@ -72,37 +72,37 @@
 // let result1=myczns.filter((item, index) => myczns.indexOf(item)== index)
 // console.log("duplicated items removed: ", result1);
 
-//map()
+//14. map()
 // let number=[2.1, 0.3, 4.1]
 // let result=number.map(Math.ceil)
 // console.log("result: ", result);
 
-//every()
+//15. every()
 // function evennumber(item:any) {
 //     return item % 1 == 0;
 // }
 // let numbers=[1,3,7,9]
 // console.log("result: ", numbers.every(evennumber));
 
-//reduceRight()
+//16. reduceRight()
 // let numbers=[3,6,7]
 // let result=numbers.reduceRight( function(a,b) {return a-b} )
 // console.log("result: ", result);
 
-//reduce()
+//17. reduce()
 // let number=[20,5,10]
 // let result=number.reduce( function (a,b) { return a*b} )
 // console.log("result: ", result);
 
-//some()
+//18. some()
 // let number = [1,2,6]
 // console.log("atleast one number is even in given array", number.some(item => item % 1 == 0));
 
-//sort()
+//19. sort()
 // let numbers=[1,4,3,6,5,8,2]
 // console.log("sorted array: ", numbers.sort((a,b) => a-b));
 
-//fill()
+//20. fill()
 // const emojis=['a','b','c','d']
 // console.log("result1: ", emojis.fill('e',2,3));
 // console.log("result2: ", emojis.fill('f',1));
